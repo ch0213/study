af110cad8494b65ca0d2fd66ec80389fb270d3b1
chkim <cndghks0213@gmail.com>
2022-08-18 16:12:39 +0900

    외부 API 테스트 시 사용할 FakeController 생성

---
+++ external-api-test/src/test/java/com/example/externalapitest/fake/FakeKakaoApiController.java
@@ -0,0 +1,15 @@
+package com.example.externalapitest.fake;
+
+import com.example.externalapitest.dto.TokenResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FakeKakaoApiController {
+    public static final String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+
+    @GetMapping("/oauth/token")
+    public TokenResponse getToken() {
+        return TokenResponse.from(token);
+    }
+}
