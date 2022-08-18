33486f74089eb113893f8e588673db1ee2cf2194
chkim <cndghks0213@gmail.com>
2022-08-18 22:04:20 +0900

    DatabaseCleanupV2 추가

---
+++ external-api-test/src/test/java/com/example/externalapitest/utils/DatabaseCleanupV2.java
@@ -0,0 +1,43 @@
+package com.example.externalapitest.utils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Table;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+/**
+ * id 'org.springframework.boot' version '2.7.0' 이상일 때
+ */
+@Profile("test")
+@Component
+public class DatabaseCleanupV2 implements InitializingBean {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private List<String> tableNames;
+
+    @Override
+    public void afterPropertiesSet() {
+        tableNames = entityManager.getMetamodel().getEntities().stream()
+                .filter(it -> it.getJavaType().getDeclaredAnnotation(Table.class) != null)
+                .map(it -> it.getJavaType().getDeclaredAnnotation(Table.class).name())
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void execute() {
+        entityManager.flush();
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
+        for (String tableName : tableNames) {
+            entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
+            entityManager.createNativeQuery("ALTER TABLE " + tableName + " ALTER COLUMN ID RESTART WITH 1")
+                    .executeUpdate();
+        }
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
+    }
+}
