67882406a59b2fb62a7cf13e4fb8f2c506f2d086
chkim <cndghks0213@gmail.com>
2022-08-18 16:13:04 +0900

    회원 조회 기능 테스트

---
+++ external-api-test/src/test/java/com/example/externalapitest/MemberAcceptanceTest.java
@@ -0,0 +1,35 @@
+package com.example.externalapitest;
+
+import com.example.externalapitest.dto.MemberResponse;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class MemberAcceptanceTest extends AcceptanceTest {
+
+    @DisplayName("회원 정보 조회기능 테스트")
+    @Test
+    void getMember() {
+        MemberResponse 회원 = 회원_정보_요청(1L).as(MemberResponse.class);
+
+        assertAll(
+                () -> assertThat(회원.getId()).isEqualTo(1L),
+                () -> assertThat(회원.getName()).isEqualTo("테스트 유저"),
+                () -> assertThat(회원.getAge()).isEqualTo(25)
+        );
+    }
+
+    private ExtractableResponse<Response> 회원_정보_요청(Long id) {
+        return RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/members/" + id)
+                .then().log().all().extract();
+    }
+}
