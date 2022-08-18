73b063279f278b3f25996fd9944835b3ceab0b0e
chkim <cndghks0213@gmail.com>
2022-08-18 20:29:59 +0900

    Fake Controller에 Profile 추가

--- external-api-test/src/test/java/com/example/externalapitest/fake/FakeKakaoApiController.java
+++ external-api-test/src/test/java/com/example/externalapitest/fake/FakeKakaoApiController.java
@@ -1,9 +1,11 @@
 package com.example.externalapitest.fake;
 
 import com.example.externalapitest.dto.TokenResponse;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Profile("acceptance-test")
 @RestController
 public class FakeKakaoApiController {
     public static final String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
