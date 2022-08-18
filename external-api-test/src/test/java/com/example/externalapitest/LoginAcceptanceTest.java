b6c80f8530ce7675db0037c3c2a294abdce015b2
chkim <cndghks0213@gmail.com>
2022-08-18 16:12:52 +0900

    외부 API를 사용하는 로그인 기능 테스트

---
+++ external-api-test/src/test/java/com/example/externalapitest/LoginAcceptanceTest.java
@@ -0,0 +1,35 @@
+package com.example.externalapitest;
+
+import com.example.externalapitest.dto.TokenResponse;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+
+import static com.example.externalapitest.fake.FakeKakaoApiController.token;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("acceptance-test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class LoginAcceptanceTest {
+
+    @DisplayName("로그인 기능 테스트")
+    @Test
+    void login() {
+        String 인증토큰 = 로그인_요청().as(TokenResponse.class).getToken();
+
+        assertThat(인증토큰).isEqualTo(token);
+    }
+
+    private ExtractableResponse<Response> 로그인_요청() {
+        return RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/login")
+                .then().log().all().extract();
+    }
+}
