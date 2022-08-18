e62f0dc4bca657c2bef104a88d255650226498f2
chkim <cndghks0213@gmail.com>
2022-08-18 14:32:38 +0900

    외부 API 테스트 실습 프로젝트 생성

---
+++ external-api-test/src/main/java/com/example/externalapitest/ExternalApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.externalapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalApiTestApplication.class, args);
+	}
+
+}
---
+++ external-api-test/src/test/java/com/example/externalapitest/ExternalApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.externalapitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
