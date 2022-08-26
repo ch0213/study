b4039f8b4f9f0d6b7785515a491888d360eb207f
chkim <cndghks0213@gmail.com>
2022-08-26 11:12:21 +0900

    불필요한 import문 제거

--- external-api-test/src/test/java/com/example/externalapitest/acceptance/LoginAcceptanceTestV2.java
+++ external-api-test/src/test/java/com/example/externalapitest/acceptance/LoginAcceptanceTestV2.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
