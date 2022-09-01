b7ff2f0645157bf0d1ad43e8c139ec7f0a526a70
chkim <cndghks0213@gmail.com>
2022-09-01 17:16:59 +0900

    @Entity 추가

--- external-api-test/src/main/java/com/example/externalapitest/domain/Member.java
+++ external-api-test/src/main/java/com/example/externalapitest/domain/Member.java
@@ -1,6 +1,12 @@
 package com.example.externalapitest.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
+    @Id @GeneratedValue
     private Long id;
     private String name;
     private int age;
