bb1ad1bd7bca9d39e56de91d3febca886faf1451
chkim <cndghks0213@gmail.com>
2022-09-15 23:56:52 +0900

    로컬 환경에서 mysql, redis를 세팅하는 예제 프로젝트 생성

---
+++ local-mysql-redis-setting/src/main/java/com/example/localmysqlredissetting/LocalMysqlRedisSettingApplication.java
@@ -0,0 +1,13 @@
+package com.example.localmysqlredissetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalMysqlRedisSettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalMysqlRedisSettingApplication.class, args);
+	}
+
+}
---
+++ local-mysql-redis-setting/src/test/java/com/example/localmysqlredissetting/LocalMysqlRedisSettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.localmysqlredissetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalMysqlRedisSettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
