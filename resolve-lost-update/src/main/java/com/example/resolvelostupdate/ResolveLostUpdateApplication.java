35659bc0b3d7ef2d0d23ec04b98ea5d3efbda16d
chkim <cndghks0213@gmail.com>
2022-10-18 23:41:33 +0900

    갱신 손실 해결 실습을 위한 프로젝트 생성

---
+++ resolve-lost-update/src/main/java/com/example/resolvelostupdate/ResolveLostUpdateApplication.java
@@ -0,0 +1,13 @@
+package com.example.resolvelostupdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResolveLostUpdateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResolveLostUpdateApplication.class, args);
+	}
+
+}
